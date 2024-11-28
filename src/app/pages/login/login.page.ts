0e37783cf558f0e7c43703662404a271cbb9794e
Fernando Ovalle E. <f2.ovallesc@gmail.com>
2024-11-28 18:09:15 -0300

    Login con validaciones y pagina de registro en proceso

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonCol, IonCard, IonCardContent, IonInput, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonInputPasswordToggle } from '@ionic/angular/standalone';
+import { FormGroup, FormsModule, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonCol, IonCard, IonCardContent, IonInput, IonCardHeader, IonCardTitle, IonCardSubtitle, IonButton, IonInputPasswordToggle, IonText  } from '@ionic/angular/standalone';
+
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonInput, IonButton, IonInputPasswordToggle, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonInput, IonButton, IonInputPasswordToggle, IonText, CommonModule, FormsModule, ReactiveFormsModule]
 })    
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
+
+  //Agregar ReactiveFormsModule
+  //variable no inicializada
+  form!: FormGroup
 
   ngOnInit() {
+    //Validaciones reactivas
+    this.form = new FormGroup({
+      email: new FormControl(null,[ 
+        Validators.required, //campos requeridos
+        Validators.email //formato email requerido
+      ]),
+      password: new FormControl(null,[ 
+        Validators.required, //campos requeridos
+        Validators.minLength(8) //minimo 8 carácteres
+
+      ]),
+    })
+  }
+
+  goToRegister(){
+    this.router.navigate(['/register']); //Redirección a la ruta '/register' para el registro de usuarios
+  }
+  validar(){
+    //Si el formulario es o no valido
+    if(this.form.invalid){
+      this.form.markAllAsTouched()
+      return
+    }
+
+    this.router.navigate(['home'])
+
   }
 
 }
--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {Router} from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonInput, IonText, IonButton, IonInputPasswordToggle } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonInput, IonText, IonButton, IonInputPasswordToggle, CommonModule, FormsModule]
 })
 export class RegisterPage implements OnInit {
+  email: string = '';
+  password: string = '';
+  firstName: string = '';
+  lastName: string = '';
+  confirmPassword: string = '';
 
-  constructor() { }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  goToLogin(){
+    this.router.navigate(['/login']);
   }
 
 }
